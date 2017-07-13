462220b88702e5793e017f418f6df9940cd72ee5
Denny Hollick <denny@Dennys-MacBook-Pro.local>
2017-07-12 18:25:05 -0700

    Created load tweets function to use database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,53 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//TODO: Test Cross Site Scripting
+
 $(document).ready(() => {
-  const data = [{
-    user: {
-      name: 'Newton',
-      avatars: {
-        small: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
-        regular: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
-        large: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png',
-      },
-      handle: '@SirIsaac',
-    },
-    content: {
-      text: 'If I have seen further it is by standing on the shoulders of giants',
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: {
-        small: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png',
-        regular: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png',
-        large: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png',
-      },
-      handle: '@rd',
-    },
-    content: {
-      text: 'Je pense , donc je suis',
-    },
-    created_at: 1461113959088,
-  },
-  {
-    user: {
-      name: 'Johann von Goethe',
-      avatars: {
-        small: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png',
-        regular: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png',
-        large: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png',
-      },
-      handle: '@johann49',
-    },
-    content: {
-      text: 'Es ist nichts schrecklicher als eine tätige Unwissenheit.',
-    },
-    created_at: 1461113796368,
-  },
-  ];
+
+  function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
 
   function createTweetElement(tweetData) {
     const html = `
@@ -60,7 +23,7 @@ $(document).ready(() => {
         <span class="username">${tweetData.user.name}</span>
         <span class="handle">${tweetData.user.handle}</span>
       </header>
-      <p>${tweetData.content.text}</p>
+      <p>${escape(tweetData.content.text)}</p>
       <footer>
         <span>${tweetData.created_at}</span>
       </footer>
@@ -71,14 +34,29 @@ $(document).ready(() => {
   function renderTweets(tweetData) {
     let allTweets = '';
     for (const tweet in tweetData) {
-      const renderedTweet = createTweetElement(data[tweet]);
+      const renderedTweet = createTweetElement(tweetData[tweet]);
       allTweets += renderedTweet;
     }
-    return allTweets;
+    $('.feed').append(allTweets);
   }
 
-  const $tweet = renderTweets(data);
+  function loadTweets() {
+    let renderedTweets = '';
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function (tweetData) {
+        renderTweets(tweetData);
+      }
+    });
+  };
 
-  // Test / driver code (temporary)
-  $('.feed').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+loadTweets();
+
+
 });
+
+
+
+
+
